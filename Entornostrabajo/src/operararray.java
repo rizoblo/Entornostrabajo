ca7365f23cca2c2d037cd85116a1557a2ed3a229
diurno <diurno@pc-aula>
2019-03-06 10:51:47 +0100

    original

---
+++ Entornostrabajo/src/operararray.java
@@ -0,0 +1,19 @@
+
+public class operararray {
+
+	static int array[]= new int [10];
+	static int numeroaleatorio=0;
+	public static void main(String[] args) {
+		rellenararray();
+
+	}
+
+	public static void rellenararray() {
+		for (int i=0;i<array.length;i++) {
+			numeroaleatorio=(int)Math.round(Math.random()*19+1);
+			array[i]=numeroaleatorio;
+			System.out.println(array[i]);
+		}
+	}
+	
+}
