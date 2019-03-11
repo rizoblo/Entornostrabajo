55f11cbfbba5effbecafb3967100a07733460868
diurno <diurno@pc-aula>
2019-03-11 08:59:55 +0100

    rama1

--- Entornostrabajo/src/operararray.java
+++ Entornostrabajo/src/operararray.java
@@ -3,9 +3,11 @@ public class operararray {
 
 	static int array[]= new int [10];
 	static int numeroaleatorio=0;
+	static int suma=0;
 	public static void main(String[] args) {
 		rellenararray();
-
+		imprimirarray();
+		sumararray();
 	}
 
 	public static void rellenararray() {
@@ -16,4 +18,19 @@ public class operararray {
 		}
 	}
 	
+	public static void imprimirarray() {
+		for(int i=0;i<array.length;i++) {
+		System.out.println(array[i]);
+		
+		}
+	}
+		
+	public static void sumararray() {
+		for(int i=0;i<array.length;i++) {
+		suma+=array[i];	
+		}
+	}
+	//copia1
+		
+	//Ahora copia2
 }
